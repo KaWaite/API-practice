0c2cd0bdc0737a219bd412308ee3cefe8da321f0
KaWaite <kyle.waite@outlook.com>
2020-02-19 23:20:47 +0900

    add styling, fix reactHooks, add dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import axios from "axios";
 import "./App.css";
 
 // Material-ui components
@@ -6,14 +8,29 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
-// import specialKey from "../../key";
-
 const useStyles = makeStyles(theme => ({
   root: {
     "& > *": {
       margin: theme.spacing(4),
       width: 200
     }
+  },
+  middle: {
+    display: "flex",
+    margin: 0,
+    padding: 0,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  area: {
+    padding: "20px",
+    margin: "50px",
+    border: "1px solid black"
+  },
+  list: {
+    listStyleType: "none",
+    textAlign: "left",
+    padding: 0
   }
 }));
 
@@ -23,97 +40,80 @@ function App() {
   // fetch data from api/express
   useEffect(() => {
     fetchItems();
-    fetchName();
-    // fetchTest();
-    // fetchAPICall();
+    fetchWeatherData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // ****useEffect setting state for local api in server.js****
   const [Items, setItems] = useState([]);
 
   const fetchItems = async () => {
     const data = await fetch("/api/test");
-    // console.log(data);
     const Items = await data.json();
     setItems(Items);
-    // console.log(Items);
   };
+  // **********************************************************
 
-  // ***********TEST**********
+  // ***********external API call attempt**********
   const [Weather, setWeather] = useState({});
   const [Coord, setCoord] = useState({});
 
-  const fetchName = async () => {
-    const data = await fetch("/openWeatherMap");
-    const blargh = await data.json();
-    console.log(blargh);
-
+  const fetchWeatherData = async () => {
     const data = await fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=fd41f9c5f443147155fff01a2ba29766"
+      `http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=fd41f9c5f443147155fff01a2ba29766`
     );
-    console.log(data);
-    const weatherData = await data.json();
-    const test = weatherData.coord;
-    setWeather(weatherData);
-    setCoord(test);
-    console.log(weatherData);
+    const items = await data.json();
+    const coordinates = items.coord;
+    setWeather(items);
+    setCoord(coordinates);
   };
-
-  // EXPERIMENTAL API CALL****************************
-  // const [test, fetchTest] = useState();
-
-  // const testingTesting = async () => {
-  //   const response = await fetch("https://api-v3.igdb.com/games", {
-  //     method: "POST",
-  //     header: {
-  //       "user-key": "1b0d8d553a89e5a44baf056a37b94793"
-  //     },
-  //     body: {
-  //       "fields name; limit 10;"
-  //     }
-  //   });
-  // };
-  // const [experiment, setExperiment] = useState();
-
-  // const fetchAPICall = async () => {
-  //   const response = await fetch("/express_backend");
-  //   const body = await response.json();
-
-  //   if (response.status !== 200) {
-  //     throw Error(body.message);
-  //   }
-  //   setExperiment(body.express);
-  // };
 
   // ********************************************************
 
   return (
-    <div className="App">
-      <div className="Main">
-        <h1>IGDB API: React, Express, (eventually)MySQL(maybe)</h1>
-        <form className={classes.root} noValidate autoComplete="off">
-          <TextField id="standard-size-small" size="small" label="Standard" />
-          <Button size="small" variant="contained" color="primary">
-            Submit
-          </Button>
-        </form>
-        <h2>Items from app.get in server.js express</h2>
-        <ul>
-          {Items.map(item => (
-            <li key={item.id}>
-              {item.name} Number: {item.id}
-            </li>
-          ))}
-        </ul>
-        <h2>From external API(OWM)</h2>
-        <ul>
-          <li>{Weather.name}</li>
-          <li>
-            {Coord.lat} , {Coord.lon}
-          </li>
-        </ul>
+    <Router>
+      <div className="App">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="Main">
+              <h1>API: React, Express, (eventually)MySQL(maybe)</h1>
+              <form className={classes.root} noValidate autoComplete="off">
+                <TextField
+                  id="standard-size-small"
+                  size="small"
+                  label="Standard"
+                />
+                <Button size="small" variant="contained" color="primary">
+                  Submit
+                </Button>
+              </form>
+              <div className={classes.middle}>
+                <div className={classes.area}>
+                  <h2>Server.js express</h2>
+                  <ul className={classes.list}>
+                    {Items.map(item => (
+                      <li key={item.id}>
+                        {item.id}: {item.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className={classes.area}>
+                  <h2>From external API(OWM)</h2>
+                  <h3>{Weather.name}</h3>
+                  <h4>
+                    {Coord.lat} , {Coord.lon}
+                  </h4>
+                </div>
+              </div>
+            </div>
+          )}
+        />
+        <Route path="/test" render={() => <h1>test worked</h1>} />
       </div>
-    </div>
+    </Router>
   );
 }
 
--- server.js
+++ server.js
@@ -2,61 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/openWeatherMap", (req, res) => {
-  const test = "hello";
-  res.text(test);
-});
-
 app.get("/api/test", (req, res) => {
-  const games = [
+  const treats = [
     {
       id: 1,
-      name: "bloodyhell"
+      name: "Jellybeans"
     },
     {
       id: 2,
-      name: "number2"
+      name: "Cookies"
     },
     {
       id: 4,
-      name: "number2sadfsdf"
+      name: "Potato Chips"
     },
     {
       id: 5,
-      name: "asdfsdfnumber2"
+      name: "Ice Cream"
     }
   ];
-  res.json(games);
+  res.json(treats);
 });
 
 // Assign port
 const port = 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-// *************************
-//***** MISC code, not working
-// igdb API
-// const igdb = require("igdb-api-node").default;
-// const APIkey = "./key";
-// const client = igdb(APIkey.igdbkey);
-
-// GET ROUTES
-// app.get("/getGames", async (req, res) => {
-//   const response = await igdb()
-//     .fields(["name", "movies", "age"]) // fetches only the name, movies, and age fields
-//     .fields("name,movies,age") // same as above
-
-//     .limit(50) // limit to 50 results
-//     .offset(10) // offset results by 10
-
-//     .sort("name") // default sort direction is 'asc' (ascending)
-//     .sort("name", "desc") // sorts by name, descending
-//     .search("mario") // search for a specific name (search implementations can vary)
-
-//     .where(`first_release_date > ${new Date().getTime() / 1000}`) // filter the results
-
-//     .request("/games"); // execute the query and return a response object
-
-//   console.log(response.data);
-// });
